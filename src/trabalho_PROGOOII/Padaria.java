6296e055160a690120228d3326f729d1e4684eec
Guilherme Carvalho <guiribeiro9134@gmail.com>
2018-09-24 16:46:48 -0400

    testando mudança de repo

--- src/trabalho_PROGOOII/Padaria.java
+++ src/trabalho_PROGOOII/Padaria.java
@@ -3,6 +3,6 @@ package trabalho_PROGOOII;
 public class Padaria {
 	public static void main(String[] args) {
 		System.out.println("Padaria!");
-		System.out.println("Testando commit");
+		System.out.println("Testando commit em outro repositório");
 	}
 }
