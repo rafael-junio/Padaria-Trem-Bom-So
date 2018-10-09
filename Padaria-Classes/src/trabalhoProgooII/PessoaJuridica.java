308732f8ac8081174b68ad5a58a494995df963fa
Guilherme Carvalho <guiribeiro9134@gmail.com>
2018-10-09 09:36:06 -0400

    Criei pessoa jurídica

--- Padaria-Classes/src/trabalhoProgooII/PessoaJuridica.java
+++ Padaria-Classes/src/trabalhoProgooII/PessoaJuridica.java
@@ -1,5 +1,39 @@
 package trabalhoProgooII;
 
-public class Pessoas {
+public class PessoaJuridica {
+	protected String nome;
+	protected String endereco;
+	protected String cnpj;
+	
+	public PessoaJuridica(String nome, String endereco, String cnpj) {
+		this.nome = nome;
+		this.endereco = endereco;
+		this.cnpj = cnpj;
+	}
 
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+	
+	
 }
