ef67fc09a8a8230e99bdd4e7a91227548edd41ba
Guilherme Carvalho <guiribeiro9134@gmail.com>
2018-10-14 12:13:32 -0400

    Alterações quase finais

--- Padaria-Teste/src/fornecedores/FornecedorOcasional.java
+++
@@ -1,9 +0,0 @@
-package fornecedores;
-
-public final class FornecedorOcasional extends Fornecedor{
-
-    public FornecedorOcasional(String nome, String endereco, String cnpj) {
-	super(nome, endereco, cnpj);
-    }
-
-}
